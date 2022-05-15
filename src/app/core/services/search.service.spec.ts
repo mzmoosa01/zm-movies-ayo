1e01c6d5337fd6b08f75c918c04255d3db1822a1
Mohammed Zubair Moosa <mzmoosa01@gmail.com>
2022-05-15 18:18:27 +0200

    chore: search service unit tests

--- src/app/core/services/search.service.spec.ts
+++ src/app/core/services/search.service.spec.ts
@@ -3,6 +3,13 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { catchError, of } from 'rxjs';
+import { getShowMock } from 'src/app/mocks/get-show-response.mock';
+import {
+  searchResponseErrorMock,
+  searchResponseMock,
+} from 'src/app/mocks/search-response.mock';
+import { SearchResponse } from 'src/app/models/search-response.model';
 
 import { environment } from 'src/environments/environment';
 import { SearchService } from './search.service';
@@ -26,9 +33,59 @@ describe('SearchService', () => {
 
   describe('searchShow()', () => {
     it('should build the query string correctly without opional params', () => {
-      service.searchShow('title', 'movie').subscribe();
+      service.searchShow('title', 'movie').subscribe((res) => {
+        expect(res).toEqual({
+          results: searchResponseMock.Search,
+          totalResults: Number(searchResponseMock.totalResults),
+        });
+      });
 
-      httpController.expectOne(`${api}&s=*title*&type=movie&page=1`);
+      const req = httpController.expectOne(
+        `${api}&s=*title*&type=movie&page=1`
+      );
+      req.flush(searchResponseMock);
+    });
+
+    it('should build the query string correctly for all params', () => {
+      service.searchShow('title', 'series', '2022', 3).subscribe((res) => {
+        expect(res).toEqual({
+          results: searchResponseMock.Search,
+          totalResults: Number(searchResponseMock.totalResults),
+        });
+      });
+
+      const req = httpController.expectOne(
+        `${api}&s=*title*&type=series&page=3&y=2022`
+      );
+      req.flush(searchResponseMock);
+    });
+
+    it('should throw an error for error response', () => {
+      service
+        .searchShow('title', 'movie')
+        .pipe(
+          catchError((err) => {
+            expect(err.message).toBe('Too many results.');
+            return of(undefined);
+          })
+        )
+        .subscribe();
+
+      const req = httpController.expectOne(
+        `${api}&s=*title*&type=movie&page=1`
+      );
+      req.flush(searchResponseErrorMock);
+    });
+  });
+
+  describe('getShow', () => {
+    it('should submit the correct Id to the request', () => {
+      const imdbID = '1234';
+      service.getShow(imdbID).subscribe((resp) => {
+        expect(resp).toEqual(getShowMock);
+      });
+      const req = httpController.expectOne(`${api}&plot=full&i=${imdbID}`);
+      req.flush(getShowMock);
     });
   });
 });
---
+++ src/app/mocks/search-response.mock.ts
@@ -0,0 +1,91 @@
+export const searchResponseMock = {
+  Search: [
+    {
+      Title: 'The Avengers',
+      Year: '2012',
+      imdbID: 'tt0848228',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Avengers: Endgame',
+      Year: '2019',
+      imdbID: 'tt4154796',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Avengers: Infinity War',
+      Year: '2018',
+      imdbID: 'tt4154756',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Avengers: Age of Ultron',
+      Year: '2015',
+      imdbID: 'tt2395427',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Captain America: The First Avenger',
+      Year: '2011',
+      imdbID: 'tt0458339',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_SX300.jpg',
+    },
+    {
+      Title: 'The Avengers',
+      Year: '1998',
+      imdbID: 'tt0118661',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg',
+    },
+    {
+      Title: 'The Toxic Avenger',
+      Year: '1984',
+      imdbID: 'tt0090190',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BNzViNmQ5MTYtMmI4Yy00N2Y2LTg4NWUtYWU3MThkMTVjNjk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Avengement',
+      Year: '2019',
+      imdbID: 'tt8836988',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BZDNmOWM5ODAtZWQ4YS00MGM3LWFlZjgtNmU1ZDM4YmFkNjczXkEyXkFqcGdeQXVyNzIzMTc2Mw@@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Ultimate Avengers: The Movie',
+      Year: '2006',
+      imdbID: 'tt0491703',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTYyMjk0NTMwMl5BMl5BanBnXkFtZTgwNzY0NjAwNzE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Ultimate Avengers II',
+      Year: '2006',
+      imdbID: 'tt0803093',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+    },
+  ],
+  totalResults: '250',
+  Response: 'True',
+};
+
+export const searchResponseErrorMock = {
+  Response: 'False',
+  Error: 'Too many results.',
+};
