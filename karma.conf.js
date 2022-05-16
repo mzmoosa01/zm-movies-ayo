2f8c17e61e16f73b2f47e7602313f4525bd36f9d
Mohammed Zubair Moosa <mzmoosa01@gmail.com>
2022-05-16 18:30:18 +0200

    chore: rename application to zm-movies-ayo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ayo-movie-pwa'),
+      dir: require('path').join(__dirname, './coverage/zm-movies-ayo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
