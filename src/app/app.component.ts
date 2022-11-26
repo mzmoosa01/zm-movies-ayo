808b6d0f42e6d0ab6689045568c562cca6f5cecd
Zubair Moosa <mzmoosa01@gmail.com>
2022-11-26 21:26:47 +0200

    Set up moe learning in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { SettingsFacade } from './core/facades/settings.facade';
 })
 export class AppComponent {
   constructor(public readonly settingsFacade: SettingsFacade) {}
+
+  getTime() {
+    //Return time as HH:MM:SS e.g. 21:30:23
+    return Date.now();
+  }
 }
